057f4aeb4cff2e454de7438e8a4c5ba8c6a91703
Joseph Goldstone <jgoldstone@arri.com>
2020-08-10 18:24:53 -0700

    Obsolete files; check in before moving them into obsolete directory.

--- image_sequence.py
+++ image_sequence.py
@@ -6,8 +6,11 @@ Sequence of frames in a single directory
 Class for identifying sequence(s) in a directory, frame ranges, etc.
 
 """
-
+import re
 from pathlib import Path
+from itertools import groupby
+from operator import itemgetter
+from fileseq import findSequencesOnDisk
 
 __author__ = 'Joseph Goldstone'
 __copyright__ = 'Copyright (C) 2020 Arnold & Richter Cine Technik GmbH & Co. Betriebs KG'
@@ -16,62 +19,114 @@ __maintainer__ = 'Joseph Goldstone'
 __email__ = 'jgoldstone@arri.com'
 __status__ = 'Experimental'
 
+SEQ_SUFFIXES = ['ari', 'dpx']
+CLIP_SUFFIXES = ['mov', 'mxf']
+SUFFIXES = SEQ_SUFFIXES.append(CLIP_SUFFIXES)
+
+
+def _contiguous_ranges(frame_numbers):
+    """Yields range objects the union of which bijectively maps to a set of supplied integers
+
+    Parameters
+    ----------
+    frame_numbers : iterable
+        sequence of integer values
+
+    Returns
+    ------
+    List
+        List of range objects each covering a contiguous, non-overlapping set of frames, with no adjacent ranges
+
+    References
+    ----------
+        "Detecting consecutive integers in a list",
+        https://stackoverflow.com/questions/2361945/detecting-consecutive-integers-in-a-list
+        TODO shorten _contiguous_ranges by only accessing first element and size of map (if possible)
+
+    """
+    ranges = []
+    # TODO (hard) modify _contiguous_frame_number_range_generator to detect non-unit frame increment(s)
+    for k, g in groupby(enumerate(frame_numbers), lambda ix: ix[0] - ix[1]):
+        elems = []
+        for thing in g:
+            elem = thing[1]
+            elems.append(elem)
+        ranges.append(range(elems[0], elems[0] + len(elems)))
+    return ranges
+
+
+
+
+
 
 class ImageSequence:
 
-    def __init__(self, dir, name, suffix, start=0, end=1, inc=1, frame_digits=None):
-        self._dir = Path(dir)
-        self._name = name
-        self._suffix = suffix
-        # TODO check that start, end, inc are numeric
-        self._start = start
-        self._end = end # n.b. follows convention of 'range' &c in that it's one past the last element in a sequence
-        self._inc = inc
-        self._frame_digits = frame_digits
-
-    @property
-    def dir(self):
-        return self._dir
-
-    @property
-    def start(self):
-        return self._start
-
-    @property
-    def end(self):
-        return self._end
-
-    @property
-    def inc(self):
-        return self._inc
+    def __init__(self, dir_path, name, suffix, frame_range, frame_digits):
+        """Create ImageSequence; values of None for name, suffix, start, end or frameD_digits mean the field is wildcard
+        """
+        self.dir_path = Path(dir_path)
+        self.name = name
+        self.suffix = suffix
+        self.frame_range = frame_range
+        self.frame_digits = frame_digits
 
     def path_for_frame(self, frame):
-        if not self._frame_digits:
+        if not self.frame_digits:
             raise IndexError(f"path for frame {frame} requested, but image sequence's frame_digits attribute is None")
-        p = self._dir / f"{self._name}.{frame:0{self._frame_digits}}.{self._suffix}"
+        p = self.dir_path / f"{self.name}.{frame:0{self.frame_digits}}.{self.suffix}"
         return p
 
-    def sequences_in_dir(self):
-        seqs = []
-        f = self._start
-        seq_start_frame = None
-        last_seen = None
-        looking_for_start = True
-        while f < self._end:
-            p = self.path_for_frame(f)
-            if p.exists() and p.is_file():
-                if looking_for_start:
-                    seq_start_frame = f
-                    looking_for_start = False
-                last_seen = f
+    @staticmethod
+    def family_member_from_components(components):
+        integer_pattern = r" *([0-9]+)"  # urk, carelessness w/leading spaces by writer allowed
+        integer_regex = re.compile(integer_pattern)
+        clan_name = None
+        frame_width = None
+        frame_number = None
+        if len(components) == 1:
+            frame_number_match = integer_regex.match(components[0])
+            if frame_number_match:
+                # 14.dpx
+                frame_width = len(frame_number_match.group())
             else:
-                if not looking_for_start:
-                    seq_end_frame = last_seen
-                    seqs.append((seq_start_frame, last_seen))
-                    seq_start_frame = None
-                    last_seen = None
-                    looking_for_start = True
-            f += self._inc
-        if not looking_for_start and seq_start_frame is not None:
-            seqs.append((seq_start_frame, last_seen))
-        return seqs
+                # very common case: my_birthday_party.mxf, for example
+                # also very common: my_frame_grab.dpx
+                clan_name = components[0]
+        else:
+            frame_number_match = integer_regex.match(components[-1])
+            if frame_number_match:
+                #   foo.03.exr
+                #   will.i.am.3.mxf
+                #   sometimes_bad_things_happen. 17.dpx
+                clan_name = components[0]
+                frame_width = len(frame_number_match.group())
+                frame_number = int(frame_number_match.group())
+            else:
+                # don't get involved with these
+                #   mr.smith.dpx
+                #   waiting.4.godot.mov
+                clan_name = '.'.join(components)
+        return clan_name, frame_width, frame_number
+
+    @staticmethod
+    def image_sequences_in_dir(dir_path):
+        sequences = []
+        for suffix in SEQ_SUFFIXES:
+            # first pass: categorize all paths of a given suffix by base name
+            # n.b. this excludes files or directories of the form '.foo', i.e.
+            # classic 'hidden' files or directories
+            clans_with_suffix = {}
+            paths = Path(dir_path).glob(f"*+(.*?).{suffix}")
+            for path in paths:
+                (clan_name, frame_width, frame_number) \
+                    = ImageSequence.family_member_from_components(path.stem.split('.'))
+                if clan_name not in clans_with_suffix:
+                    clans_with_suffix[clan_name] = {}
+                clans_with_suffix[clan_name][frame_width].append(frame_number)
+            for (clan_name, families) in clans_with_suffix.items():
+                for (frame_width, frame_numbers) in families.items():
+                    for frame_range in _contiguous_ranges(frame_numbers):
+                        sequences.append(ImageSequence(dir_path, clan_name, suffix, frame_range,
+                                                       frame_digits=frame_width))
+        return sequences
+
--- test_image_sequence.py
+++ test_image_sequence.py
@@ -1,7 +1,8 @@
 import unittest
 import uuid
 from pathlib import Path
-from image_sequence import ImageSequence
+from typing import Iterable
+from image_sequence import ImageSequence, _contiguous_ranges
 import numpy as np
 
 TEST_DIR_PATH = f"/tmp/py_unit_{uuid.uuid1()}"
@@ -10,6 +11,7 @@ TEST_FILE_SUFFIX = "exr"
 TEST_SEQ_START = 0
 TEST_SEQ_END = 3
 TEST_SEQ_INC = 1
+TEST_SEQ_FRAME_RANGE = range(TEST_SEQ_START, TEST_SEQ_END, TEST_SEQ_INC)
 TEST_SEQ_FRAME_DIGITS = 3
 
 
@@ -18,8 +20,8 @@ class MyTestCase(unittest.TestCase):
     def setUp(self):
         # rather dubious to have the test suite setup depend on something it's defining.
         # TODO re-implement test_image_sequence.py setUp() and tearDown() without using ImageSequence itself
-        self.test_seq = ImageSequence(TEST_DIR_PATH, TEST_FILE_NAME, TEST_FILE_SUFFIX, start=TEST_SEQ_START, end=TEST_SEQ_END, inc=TEST_SEQ_INC, frame_digits=TEST_SEQ_FRAME_DIGITS)
-        d = Path(self.test_seq._dir)
+        self.test_seq = ImageSequence(TEST_DIR_PATH, TEST_FILE_NAME, TEST_FILE_SUFFIX, range(TEST_SEQ_START, TEST_SEQ_END, TEST_SEQ_INC), TEST_SEQ_FRAME_DIGITS)
+        d = Path(self.test_seq.dir_path)
         if not d.exists():
             d.mkdir(mode=0o777)
             print(f"created directory `{d}'")
@@ -30,9 +32,9 @@ class MyTestCase(unittest.TestCase):
             print(f"\tcreated file `{p}'")
             p.touch()
 
-    def recursive_remove_dir_contents(self, dir, indentation_level):
-        indentation = '\t'*indentation_level
-        dir_contents = dir.glob('*')
+    def recursive_remove_dir_contents(self, fs_dir, indentation_level):
+        indentation = '\t' * indentation_level
+        dir_contents = fs_dir.glob('*')
         for content in dir_contents:
             if content.is_dir():
                 self.recursive_remove_dir_contents(content, indentation_level + 1)
@@ -42,30 +44,26 @@ class MyTestCase(unittest.TestCase):
                 content.unlink()
                 print(f"{indentation}removed file `{content}'")
 
-
     def tearDown(self):
-        d = Path(self.test_seq._dir)
+        d = Path(self.test_seq.dir_path)
         self.recursive_remove_dir_contents(d, 1)
         d.rmdir()
         print(f"removed directory `{d}"'')
 
     def test_ctor(self):
-        self.assertEqual(TEST_DIR_PATH, str(self.test_seq.dir))
-        self.assertEqual(TEST_FILE_NAME, self.test_seq._name)
-        self.assertEqual(TEST_FILE_SUFFIX, self.test_seq._suffix)
-        self.assertEqual(TEST_SEQ_START, self.test_seq.start)
-        self.assertEqual(TEST_SEQ_END, self.test_seq.end)
-        self.assertEqual(TEST_SEQ_INC, self.test_seq.inc)
-        self.assertEqual(TEST_SEQ_FRAME_DIGITS, self.test_seq._frame_digits)
+        self.assertEqual(TEST_DIR_PATH, str(self.test_seq.dir_path))
+        self.assertEqual(TEST_FILE_NAME, self.test_seq.name)
+        self.assertEqual(TEST_FILE_SUFFIX, self.test_seq.suffix)
+        self.assertEqual(TEST_SEQ_FRAME_RANGE, self.test_seq.frame_range)
+        self.assertEqual(TEST_SEQ_FRAME_DIGITS, self.test_seq.frame_digits)
 
     def test_path_for_frame(self):
-        seq = ImageSequence(TEST_DIR_PATH, TEST_FILE_NAME, TEST_FILE_SUFFIX, start=TEST_SEQ_START, end=TEST_SEQ_END,
-                            inc=TEST_SEQ_INC, frame_digits=TEST_SEQ_FRAME_DIGITS)
+        seq = ImageSequence(TEST_DIR_PATH, TEST_FILE_NAME, TEST_FILE_SUFFIX, range(TEST_SEQ_START, TEST_SEQ_END, TEST_SEQ_INC), TEST_SEQ_FRAME_DIGITS)
         path_for_frame = seq.path_for_frame(1)
         self.assertTrue(f"{TEST_DIR_PATH}/{TEST_FILE_NAME}.001.exr" == str(path_for_frame))
 
-
-    def make_subdir_with_sequence(self, dir_path, base_name, seq, frame_number_width, suffix):
+    @staticmethod
+    def make_subdir_with_sequence(dir_path: str, base_name: str, seq: Iterable, frame_number_width: int, suffix: str) -> Path:
         """Make a temporary subdirectory directory with 0-length frames according to supplied arguments
 
         Parameters
@@ -104,14 +102,12 @@ class MyTestCase(unittest.TestCase):
         dir_path_as_path = Path(dir_path)
         tmp_subdir_path = dir_path_as_path / str(uuid.uuid1())
         assert tmp_subdir_path is not dir_path_as_path
-        if base_name is not None:
-            name = base_name
         if frame_number_width is not None:
             assert frame_number_width > 0
         else:
-            assert len(seq) == 0
+            assert not seq
         tmp_subdir_path.mkdir()
-        if len(seq) != 0:
+        if seq:
             assert sum(np.array(seq) < 0) == 0
             for frame_number in seq:
                 name = ""
@@ -125,13 +121,13 @@ class MyTestCase(unittest.TestCase):
                 tmp_file_path.touch()
         return tmp_subdir_path
 
-    # def remove_dir_and_any_contained_files(self, dir_path):
-    #     assert(dir_path.exists())
-    #     assert(dir_path.is_dir())
+    # def remove_dir_and_any_contained_files(self, root_dir):
+    #     assert(root_dir.exists())
+    #     assert(root_dir.is_dir())
     #     contained_files = dir_path_exists.glob('*')
     #     for contained_file in contained_files:
     #         contained_file.unlink()
-    #     dir_path.rmdir()
+    #     root_dir.rmdir()
 
     # unit tests for unit test helper function!
     def test_msws_raises_on_0_width(self):
@@ -160,6 +156,40 @@ class MyTestCase(unittest.TestCase):
         self.assertTrue(Path(tmp_subdir_path / 'x.0005.exr').exists())
         self.assertTrue(Path(tmp_subdir_path / 'x.0007.exr').exists())
 
+    def test_contiguous_range_single_range(self):
+        reference = range(2,7)
+        actual = _contiguous_ranges(list(reference))[0]
+        self.assertEqual(reference, actual)
+
+    def _check_round_trip(self, dir_path, name, suffix, frame_ranges, frame_digits):
+        flattened_frame_seq = []
+        for frame_range in frame_ranges:
+            flattened_frame_seq.extend(frame_range)
+        candidates = ImageSequence.image_sequences_in_dir(dir_path)
+        candidate_flattened_frame_seq = []
+        for candidate in candidates:
+            self.assertEqual(dir_path, candidate.dir_path)
+            self.assertEqual(name, candidate.name)
+            self.assertEqual(suffix, candidate.suffix)
+            candidate_flattened_frame_seq.extend(candidate.frame_range)
+            self.assertEqual(frame_digits, candidate.frame_digits)
+        self.assertEqual(flattened_frame_seq, candidate_flattened_frame_seq)
+
+    # foo.0001.exr should produce [('foo', Range(1,2), 4, "exr")]
+    def test_single_singleton_sequence(self):
+        self._check_round_trip(TEST_DIR_PATH, TEST_FILE_NAME, TEST_FILE_SUFFIX, [range(1, 2)], 4)
+
+    def test_single_adjacent_sequence(self):
+        self._check_round_trip(TEST_DIR_PATH, TEST_FILE_NAME, TEST_FILE_SUFFIX, [range(1, 7)], 4)
+
+    def test_adjacent_singleton_and_continuous_sequence(self):
+        self._check_round_trip(TEST_DIR_PATH, TEST_FILE_NAME, TEST_FILE_SUFFIX, [range(1, 2), range(3, 7)], 4)
+
+    def test_adjacent_sequences(self):
+        self._check_round_trip(TEST_DIR_PATH, TEST_FILE_NAME, TEST_FILE_SUFFIX, [range(1, 3), range(5, 7)], 4)
+
+    def test_adjacent_continuous_sequence_and_singleton(self):
+        self._check_round_trip(TEST_DIR_PATH, TEST_FILE_NAME, TEST_FILE_SUFFIX, [range(1, 5), range(6, 7)], 4)
 
 if __name__ == '__main__':
     unittest.main()
